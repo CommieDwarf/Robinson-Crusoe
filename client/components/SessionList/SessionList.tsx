5f845c607b4d1bef65cdad16c6111760e20a2b0d
Konrad <tosiek22@gmail.com>
2024-04-05 17:58:29 +0200

    minor fix

--- client/components/SessionList/SessionList.tsx
+++ client/components/SessionList/SessionList.tsx
@@ -13,49 +13,38 @@ export function SessionList() {
                 playerAmount={2}
                 maxPlayerAmount={4}
                 scenario={"castaways"}
-                host={"AnalnyDewastator"}
+                host={"Maciek"}
                 password={true}
             />
             <Session
-                name={"AAAAAAAAAAAAAAAAAAAAAAAAA"}
+                name={"Lubie grać w minecrafta"}
                 playerAmount={2}
                 maxPlayerAmount={4}
                 scenario={"castaways"}
-                host={"AAAAAAAAAAAAAAAA"}
+                host={"Maciek"}
                 password={true}
-            />
-            <Session
-                name={"Iksde"}
-                playerAmount={1}
-                maxPlayerAmount={2}
-                scenario={"castaways"}
-                host={"NiemyMichałek76"}
-                password={false}
-            />
-            <Session
-                name={"A kto by go przenosił, taki ciężki"}
-                playerAmount={4}
-                maxPlayerAmount={4}
-                scenario={"castaways"}
-                host={"Papiesz"}
-                password={false}
-            />
-            <Session
-                name={"FxD"}
-                playerAmount={3}
-                maxPlayerAmount={3}
-                scenario={"castaways"}
-                host={"okrutnik"}
-                password={true}
-            />
-            <Session
-                name={"wbijaj damian"}
-                playerAmount={1}
-                maxPlayerAmount={4}
-                scenario={"castaways"}
-                host={"3$@#CO"}
-                password={true}
-            />
+            /> <Session
+            name={"Lubie grać w minecrafta"}
+            playerAmount={2}
+            maxPlayerAmount={4}
+            scenario={"castaways"}
+            host={"Maciek"}
+            password={true}
+        /> <Session
+            name={"Lubie grać w minecrafta"}
+            playerAmount={2}
+            maxPlayerAmount={4}
+            scenario={"castaways"}
+            host={"Maciek"}
+            password={true}
+        /> <Session
+            name={"Lubie grać w minecrafta"}
+            playerAmount={2}
+            maxPlayerAmount={4}
+            scenario={"castaways"}
+            host={"Maciek"}
+            password={true}
+        />
         </div>
     </div>
 }
