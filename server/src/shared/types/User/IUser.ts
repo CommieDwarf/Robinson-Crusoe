9aa8a4b86c72e9ad49ee678572a579ba9515a8fc
CommieDwarf <tosiek22@gmail.com>
2024-09-27 15:28:19 +0200

    fix vercel build error9

--- server/src/shared/types/User/IUser.ts
+++ server/src/shared/types/User/IUser.ts
@@ -1,19 +1,18 @@
 import {SessionData} from "@shared/types/Session/Session";
-import {Socket} from "socket.io";
 
 export interface IUser {
     id: string,
-    sockets: Socket[],
+    sockets: any[],
     username: string,
     activeSessions: SessionData[],
     addActiveSession: (session: SessionData) => void;
     removeSession: (sessionId: string) => void;
     leaveLobbies: () => string[] | Promise<string[]>;
     latency: number;
-    addSocket: (socket: Socket) => void;
+    addSocket: (socket: any) => void;
     getSession: (sessionId: string) => SessionData;
     getPlaceHolder: () => UserPlaceHolder
-    onSocketDisconnect: (socket: Socket, onLobbyLeave?: (sessionIds: string[]) => void) => void;
+    onSocketDisconnect: (socket: any, onLobbyLeave?: (sessionIds: string[]) => void) => void;
 }
 
 export interface UserPlaceHolder {
