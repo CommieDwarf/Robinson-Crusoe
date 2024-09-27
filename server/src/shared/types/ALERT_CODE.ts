4560df414d8cbf83d4442c5ce6849276f2013bbc
CommieDwarf <tosiek22@gmail.com>
2024-09-27 14:34:00 +0200

    testing

--- server/src/shared/types/ALERT_CODE.ts
+++ server/src/shared/types/ALERT_CODE.ts
@@ -5,4 +5,3 @@ export enum ALERT_CODE {
     PLAYERS_NOT_READY_FOR_ACTION = "players not ready for action",
     CHANGE_PHASE_IS_PRIME_PLAYER_ACTION = "this is prime player action"
 }
-ncvzxdnvlzkjxnjvkl
