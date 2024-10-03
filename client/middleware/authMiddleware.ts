e03dd6c65699394234547ed0721c680580c1d59e
CommieDwarf <tosiek22@gmail.com>
2024-10-03 11:53:12 +0200

    test

--- client/middleware/authMiddleware.ts
+++ client/middleware/authMiddleware.ts
@@ -3,11 +3,13 @@ import {NextRequest, NextResponse} from "next/server";
 
 export function authMiddleware(request: NextRequest) {
     const {pathname} = request.nextUrl;
-    const notProtectedPaths = ['/signIn', '/signOut', "/u"];
+    const notProtectedPaths = ['/signIn', '/signOut', "/signUp"];
     const requiresAuth = !notProtectedPaths.some((path) => pathname.startsWith(path));
 
     if (requiresAuth) {
         const cookie = request.cookies.get("Authorization");
+        console.log("cookie - ", cookie);
+        console.log("valid -", cookie && isTokenValid(cookie.value));
         if (!cookie || !isTokenValid(cookie.value)) {
             return NextResponse.redirect(new URL('/signIn', request.url));
         }
