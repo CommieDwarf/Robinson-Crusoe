0f9925523d949cccdecacf9f665974b76fdd4912
CommieDwarf <tosiek22@gmail.com>
2024-10-08 09:20:24 +0200

    test

--- client/config.ts
+++ client/config.ts
@@ -1,10 +1,8 @@
 const dev = process.env.NODE_ENV !== "production";
 
 const config = {
-    // SERVER_URL: dev ? process.env.DEV_SERVER_URL  || "dev server url" : process.env.PROD_SERVER_URL || "prod server url"
-    SERVER_URL: "http://localhost:3030"
+    SERVER_URL: dev ? process.env.DEV_SERVER_URL  || "dev server url" : process.env.PROD_SERVER_URL || "prod server url"
 }
 
-console.log("config", config);
 
 export default config;
